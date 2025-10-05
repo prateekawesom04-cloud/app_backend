3c335fb96308bb199abe1643270bd7f9c3262e9f
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-04 19:06:32 -0700

    updates

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    //
+}
